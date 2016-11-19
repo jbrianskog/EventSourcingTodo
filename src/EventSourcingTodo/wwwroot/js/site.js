41dbd7087f7e6b37fa462b58dff1b6157728a2cf
Brian Skog <jbrianskog@gmail.com>
2016-11-19 04:18:49 -0800

    fix to-do actions panel not hiding when clicking on another to-do's todoActionsBtn

--- src/EventSourcingTodo/wwwroot/js/site.js
+++ src/EventSourcingTodo/wwwroot/js/site.js
@@ -20,7 +20,12 @@ $(function () {
     jsViewBag.csrfToken = $("input[name=__RequestVerificationToken]").first().val();
 
     // Delegated event handler
-    $("#todoListAjaxTarget").on("click", ".completeTodoBtn", function () {
+    $("#todoListAjaxTarget").on("click", ".completeTodoBtn", function (event) {
+        var todoActionsBtn = $(this).find(".todoActionsBtn")[0];
+        // short-circuit if this event has bubbled up from the todoActionsBtn (which is inside (un)complete todo buttons)
+        if (todoActionsBtn === event.target || $.contains(todoActionsBtn, event.target)) {
+            return;
+        };
         var url = jsViewBag.urlCompleteTodo,
             requestData = {
                 TodoId: this.getAttribute("data-estd-todo-id"),
@@ -30,12 +35,17 @@ $(function () {
             .done(todoListReplacingAjaxSubmitOnDone);
     });
     // Delegated event handler
-    $("#todoListAjaxTarget").on("click", ".uncompleteTodoBtn", function () {
+    $("#todoListAjaxTarget").on("click", ".uncompleteTodoBtn", function (event) {
+        var todoActionsBtn = $(this).find(".todoActionsBtn")[0];
+        // short-circuit if this event has bubbled up from the todoActionsBtn (which is inside (un)complete todo buttons)
+        if (todoActionsBtn === event.target || $.contains(todoActionsBtn, event.target)) {
+            return;
+        };
         var url = jsViewBag.urlUncompleteTodo,
             requestData = {
-                TodoId: this.getAttribute("data-estd-todo-id"),
-                __RequestVerificationToken: jsViewBag.csrfToken
-            }
+            TodoId: this.getAttribute("data-estd-todo-id"),
+            __RequestVerificationToken: jsViewBag.csrfToken
+        }
         $.post(url, requestData)
             .done(todoListReplacingAjaxSubmitOnDone);
     });
@@ -73,19 +83,25 @@ $(function () {
     });
 
     // Delegated event handler
-    $("#todoListAjaxTarget").on("click", ".todoActionsBtn", function () {
+    $("#todoListAjaxTarget").on("click", ".todoActionsBtn", function (event1) {
         var $panelDefault = $(this).closest(".todoPanelDefault"),
             $actionsPanel = $panelDefault.next(),
-            $bothPanels = $panelDefault.add($actionsPanel);
+            $bothPanels = $panelDefault.add($actionsPanel),
+            actionsBtnGroup = $actionsPanel.find(".todoActionsPanelBtnGroup")[0],
+            eventNamespace = "click.todoActionsPanelClose" + event1.timeStamp.toString().replace(".", "");
         $bothPanels.toggle();
-
-        $(document).on("click.todoActionsPanelClose", function (event) {
-            if (!$.contains($actionsPanel.find(".todoActionsPanelBtnGroup")[0], event.target)) {
+        
+        // eventNamespace is unique (probably) so each instance of this handler can only detach itself
+        $(document).on(eventNamespace, function (event2) {
+            // short-circuit when handler is triggered by event that created it
+            if (event1.originalEvent === event2.originalEvent) {
+                return;
+            };
+            if (!$.contains(actionsBtnGroup, event.target)) {
                 $bothPanels.toggle();
             }
-            $(document).off("click.todoActionsPanelClose");
+            $(document).off(eventNamespace);
         });
-        return false;
     });
     // Delegated event handler
     $("#todoListAjaxTarget").on("click", ".renameTodoBtn", function () {
